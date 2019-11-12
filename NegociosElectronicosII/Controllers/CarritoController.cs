b0342c43ec622342e509cb3c8a40788c7fd15cdd
HP <HP@LAPTOP-E55NB4HC>
2019-11-12 15:18:47 -0600

    error

---
+++ NegociosElectronicosII/Controllers/CarritoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NegociosElectronicosII.Controllers
+{
+    public class CarritoController : BaseController 
+    {
+        // GET: Carrito
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult AgregarCarro(int id, int constante)
+        {
+            return Json(new { Success = true, Message = id }, JsonRequestBehavior.DenyGet);
+        }
+    }
+}
