5a2739d81cf32daf057a6a022c75840b7b325f21
HP <HP@LAPTOP-E55NB4HC>
2019-11-12 17:27:05 -0600

    carrito

--- NegociosElectronicosII/Controllers/CarritoController.cs
+++ NegociosElectronicosII/Controllers/CarritoController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NegociosElectronicosII.Models;
 
 namespace NegociosElectronicosII.Controllers
 {
@@ -16,6 +17,18 @@ namespace NegociosElectronicosII.Controllers
         [HttpPost]
         public JsonResult AgregarCarro(int id, int constante)
         {
+            
+                NE_Carrito nE_Carrito = new NE_Carrito()
+                {
+                    RecordDate = DateTime.Now,
+                    UsuarioId = 1,
+                    ProductoId = id,
+                    VehiculoId = 1,
+
+                };
+            
+            db.NE_Carrito.Add(nE_Carrito);
+            db.SaveChanges();
             return Json(new { Success = true, Message = id }, JsonRequestBehavior.DenyGet);
         }
     }
