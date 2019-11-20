3ab5e66e3f2cbcfabbbb14af937c8481a13a9446
HP <HP@LAPTOP-E55NB4HC>
2019-11-19 19:56:02 -0600

    agregarimagen

--- NegociosElectronicosII/Controllers/VehiculoController.cs
+++ NegociosElectronicosII/Controllers/VehiculoController.cs
@@ -243,5 +243,71 @@ namespace NegociosElectronicosII.Controllers
             }
             base.Dispose(disposing);
         }
+
+        // GET: Carrusell/Create
+        public ActionResult ImagenCreate()
+        {
+            return View();
+        }
+
+        // POST: Carrusell/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ImagenCreate([Bind(Include = "CarruselId,Posicion,Texto,Descripcion,Ruta,Extension,NombreArchivo,Activo")] NE_Carrusel nE_Carrusel)
+        {
+
+            try
+            {
+                string mensaje;
+                HttpPostedFileBase postedFile = Request.Files[0];
+
+                if (ModelState.IsValid)
+                {
+                    if (postedFile != null && postedFile.ContentLength > 0)
+                    {
+                        IList<string> AllowedFileExtensions = new List<string> { ".jpg", ".gif", ".png" };
+                        var ext = postedFile.FileName.Substring(postedFile.FileName.LastIndexOf('.'));
+                        var extension = ext.ToLower();
+                        if (!AllowedFileExtensions.Contains(extension))
+                        {
+                            mensaje = "Porfavor actualiza la imagen a estension de tipo .jpg,.gif,.png.";
+                        }
+                        else
+                        {
+                            var name = String.Format("banner_{0}", nE_Carrusel.CarruselId);
+                            var filePath = Server.MapPath("~/Imagenes/Carrusel/" + name + extension);
+
+                            NE_Carrusel imagen = new NE_Carrusel()
+                            {
+                                NombreArchivo = name,
+                                Extension = ext,
+                                Ruta = filePath,
+                                Posicion = nE_Carrusel.Posicion,
+                                Texto = nE_Carrusel.Texto,
+                                Descripcion = nE_Carrusel.Descripcion,
+                                Activo = nE_Carrusel.Activo,
+                            };
+                            db.NE_Carrusel.Add(imagen);
+                            db.SaveChanges();
+
+                            postedFile.SaveAs(filePath);
+
+                            return RedirectToAction("Index");
+                        }
+                    }
+
+                }
+                return View(nE_Carrusel);
+            }
+            catch (Exception ex)
+            {
+                return View(nE_Carrusel);
+            }
+
+        }
+
+
     }
 }
