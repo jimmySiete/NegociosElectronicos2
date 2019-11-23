0ba1437c9e6c5b5daa8a8db507a62ca328c7f6a2
HP <HP@LAPTOP-E55NB4HC>
2019-11-22 21:44:16 -0600

    cambios

---
+++ NegociosElectronicosII/Controllers/ChangePassController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NegociosElectronicosII.Controllers
+{
+    public class ChangePassController : BaseController
+    {
+        // GET: ChangePass
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult Passwoord()
+        {
+            return PartialView();
+        }
+    }
+}
